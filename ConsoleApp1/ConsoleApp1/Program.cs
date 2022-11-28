3402eb5d5820766bdc080378d04e025eb804e7fe
ThanapronChanidkul <thanapron.c@kkumail.com>
2022-11-28 16:17:03 +0700

    nameapp

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Thanapron Chanidkul");
+Console.WriteLine("653450092-2");
+Console.WriteLine("thanapron.c@kkumail.com");
